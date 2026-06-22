cbfd032026b77baa4140cd01af038db449b90e10
pikstyle <pro.pikstyle@gmail.com>
2026-06-22 16:48:50 +0200

    style profile.jsx

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,17 +1,18 @@
 import { useState } from "react"
+import styles from './Profile.module.css'
 
 function Profile( {user, updateProfile, profile, logout} ) {
     const [username, setUsername] = useState("")
     return (
-        <div>
+        <div className={styles.div_princ}>
             <h1>Profile</h1>
             <h3>{profile?.username}</h3>
             {profile?.avatar_url && <img src={profile.avatar_url} alt="avatar"/>}
             <h2>Update le profile : </h2> {/* Affiche la photo seulement si avatar_url existe (évite le flash de la pdp Google au chargement) */}
             <label>Noueau pseudo : </label>
-            <input type="text" value={username} onChange={(e) => setUsername(e.target.value)}/>
-            <button onClick={() => updateProfile({ username, avatar_url: profile?.avatar_url })}>Save</button>
-            <button onClick={() => logout()}>Logout</button>
+            <input className={styles.inputs} type="text" value={username} onChange={(e) => setUsername(e.target.value)}/>
+            <button className={styles.button} onClick={() => updateProfile({ username, avatar_url: profile?.avatar_url })}>Save</button>
+            <button className={styles.button} onClick={() => logout()}>Logout</button>
         </div> 
     )
 }
