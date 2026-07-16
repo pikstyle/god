f179caa5a3f53c1e71356ed65edb07211c4bcb0c
pikstyle <pro.pikstyle@gmail.com>
2026-07-16 23:03:11 +0200

    new discord invite link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
         <div className={styles.container}>
             <footer className={styles.footer}>
                 <div className={styles.inner}>
-                    <a target="_blank" href="https://discord.gg/AMNYvNxfEs">Discord</a>
+                    <a target="_blank" href="https://discord.gg/gyUvG3HW9F">Discord</a>
                     <a href="mailto:pro.pikstyle@gmail.com">Contact</a>
                     <NavLink to="/cgu">CGU</NavLink>
 
