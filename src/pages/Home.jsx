59ec87e706f6255b4bb798ef19db1081088b192f
pikstyle <pro.pikstyle@gmail.com>
2026-07-24 12:41:05 +0200

    edit home content default

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,45 +15,25 @@ function Home({ isLeader, textHome, saveHomeContent, partiLeader, gameState, use
     }
     const fileInputRef = useRef(null) // avoir une ref de l'input pour upload l'image
     const defaultHome = {
-        "canvas": {
-            "h": 700,
-            "w": 1200
-        },
-        "version": 2,
-        "elements": [
-            {
-                "h": 78,
-                "w": 1141,
-                "x": 32.631193032949795,
-                "y": 72.9297081776406,
-                "id": "text1",
-                "size": 60,
-                "type": "text",
-                "color": "#ff2600",
-                "text_content": "TAKE FULL CONTROL OF THIS PAGE"
+            "canvas": {
+                "h": 700,
+                "w": 1200
             },
-            {
-                "h": 381,
-                "w": 886,
-                "x": 154.0031799345656,
-                "y": 209.77634408100252,
-                "id": "7a6e9649-6314-4f89-a161-49aafe85919a",
-                "url": "https://media0.giphy.com/media/v1.Y2lkPThkYjI3YzdmZjdtdzhnMXNtcDZwcHI0YnpzOHBlNG9rZDlwbnR3Yjdua2h1enFiNyZlcD12MV9naWZzX3NlYXJjaCZjdD1n/MMquV2oInK40V86Q7g/200.gif",
-                "type": "gif"
-            },
-            {
-                "h": 24,
-                "w": 138,
-                "x": 515.1756441369245,
-                "y": 602.5367864420216,
-                "id": "b7a0852f-1311-444d-9a31-9744e7cd3e64",
-                "size": 15,
-                "type": "text",
-                "color": "#000000",
-                "text_content": "(this could be you)"
-            }
-        ]
-    }
+            "version": 2,
+            "elements": [
+                {
+                    "h": 1,
+                    "w": 10000,
+                    "x": 75,
+                    "y": 270,
+                    "id": "text1",
+                    "size": 90,
+                    "type": "text",
+                    "color": "#000000",
+                    "text_content": "YOUR CONTENT HERE"
+                }
+            ]
+        }
 
     const contenu = gameState?.regne ? textHome : defaultHome
 
